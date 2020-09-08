380a562f7d9e50a49d6bbb4cc4ba0eba55eca2f4
favouraka <akafavour1@yahoo.com>
2020-09-08 20:45:16 +0100

    Modified web routes

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/linkstorage', function () {
     Artisan::call('storage:link');
 });
-
-Route::get('{reactRoutes}', function () {
+// here we use a catch-all route to intercept all
+// get requests on web route and return our blade file
+Route::get('/{any}', function(){
     return view('index');
-})->where('reactRoutes', '^((?!api).)*$');
+})->where('any','.*');
+
+// since we have already intercepted the get requests
+// only post requests available in the Auth::routes
+// will be avaialable
+Auth::routes();
