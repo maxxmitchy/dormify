6f5ef69861c1e055e39d3d3c4544579392768d17
favouraka <akafavour1@yahoo.com>
2020-09-09 14:39:43 +0100

    add universities relationship to city model

--- app/City.php
+++ app/City.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class City extends Model
 {
     protected $table = 'cities';
+
+    public function universities()
+    {
+        return $this->hasMany(University::class);
+    }
 }
